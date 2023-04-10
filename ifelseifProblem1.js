ed64b046648447024ec405e4b00c21c4381ea48a
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 22:47:37 +0530

    Added:code to write given number in word

---
+++ ifelseifProblem1.js
@@ -0,0 +1,29 @@
+let a = 3;
+
+function readNumber(a){  
+if (a == 0){
+console.log("Zero");
+} else if (a == 1) {
+  console.log("One");
+} else if (a == 2) {
+  console.log("Two");
+}else if (a == 3) {
+  console.log("Three");
+}else if (a == 4) {
+  console.log("Four");
+}else if (a == 5) {
+  console.log("Five");
+}else if (a == 6) {
+  console.log("Six");
+}else if (a == 7) {
+  console.log("Seven");
+}else if (a == 8) {
+  console.log("Eight");
+}else if (a == 9) {
+  console.log("Nine");
+}else {
+  console.log("Not single digit number");
+}
+}
+
+readNumber(3);
