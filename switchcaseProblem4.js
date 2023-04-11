883cd9fb6a0c198703be68f80f70edce94de1a95
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-11 17:19:33 +0530

    Added:code to take user input and does unit conversion of different length units

---
+++ switchcaseProblem4.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")();
+inputQuantity = prompt("Enter a input quantity: ");
+choice = parseInt(prompt("Enter a input: \n1.feet to inch \n2. inch to feet \n3.feet to meter\n4. meter to feet\n"));
+
+switch (choice) {
+    case 1:
+        console.log("input quantity in inches is: " + inputQuantity * 12.0);
+        break;
+    case 2:
+        console.log("input quantity in feet is: " + inputQuantity / 12.0);
+        break;
+    case 3:
+        console.log("input quantity in meter is: " + inputQuantity * 0.3048);
+        break;
+    case 4:
+        console.log("input quantity in feet is: " + inputQuantity / 0.3048);
+        break;
+    default:
+        console.log("enter valid choice");
+}
