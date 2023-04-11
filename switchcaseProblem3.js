23374858bd85ed5270003ded0660ff8c8191896e
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-11 14:20:37 +0530

    Added:code to read a number 1,10,100.. and display unit,ten.. by using switch case

---
+++ switchcaseProblem3.js
@@ -0,0 +1,15 @@
+let number = 100;
+
+switch(number){
+  case 1 : console.log("One");
+  break;
+  case 10 : console.log("Ten");
+  break;
+  case 100 : console.log("Hundred");
+  break;
+  case 1000 : console.log("Thousand");
+  break;
+  case 10000 : console.log("Ten thousand");
+  break;
+  default:console.log("Enter wrong input")
+}
